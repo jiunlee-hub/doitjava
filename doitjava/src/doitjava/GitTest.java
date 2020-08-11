8beed5fef2f2af447bd5cb57ebe62e8d1f1c5255
yn-15 <yn-15@LAPTOP-098HAMD0>
2020-08-11 14:55:33 +0900

    자바 1일차 예제

---
+++ doitjava/src/doitjava/GitTest.java
@@ -0,0 +1,9 @@
+package doitjava;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println ("깃허브 테스트");
+	}
+
+}
---
+++ doitjava/src/doitjava/Myname.java
@@ -0,0 +1,23 @@
+/**
+ * (asterisk)를 2개 사용한 주석은
+ * 도큐먼터리 주석으로 문서의 정보를
+ * 나타낼 때 사용합니다.
+ * 작성자 : 이지언
+ * 작성일 : 20.08.10.
+ */
+package doitjava;
+
+public class Myname {
+
+	public static void main(String[] args) {
+		System.out.println ("제 이름은 이지언입니다");
+		System.out.println("직업은 학생입니다.");
+		
+		
+	}
+
+}
+/*
+*첫수업 시간에 작성한 코드입니다.
+*어떤 언어를 학습할 때 최초로 화면출력을 베우게 됩니다.
+**/
