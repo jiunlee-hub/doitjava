8c750a2c9e91281f6f96865d0350fc517946e31f
jiunlee <jiunlee.dev@gmail.com>
2020-08-13 15:26:51 +0900

    chapter02 문자형 학습

---
+++ doitjava/src/chapter02/CharEx01.java
@@ -0,0 +1,25 @@
+package chapter02;
+
+public class CharEx01 {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+//		ch1 = 'BC'; //캐릭터 자료형은 1개의 문자만 입력 가능합니다.
+		int num1 = ch1;
+		System.out.println(num1); //대문자 A는 숫자65와 같습니다.
+		
+		char ch2 = 66;
+		char ch3 = 67;
+		System.out.println(ch2);
+		System.out.println(ch3);
+		
+		char ch4 = 'a';
+		int num4 = ch4;
+		System.out.println(num4);
+		
+		System.out.println('a' - 'A');
+		
+	}
+
+}
