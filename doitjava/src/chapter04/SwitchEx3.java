83e69d93cc0fdbc3ce4ba3dfe38bd9ee358e1374
jiunlee <jiunlee.dev@gmail.com>
2020-08-19 15:57:13 +0900

    chapter 04 반복문

---
+++ doitjava/src/chapter04/SwitchEx3.java
@@ -0,0 +1,27 @@
+package chapter04;
+
+public class SwitchEx3 {
+
+	public static void main(String[] args) {
+		int thisMon = 2;
+		System.out.println(thisMon + "월의 마지막 일자는 몇 일인가요?");
+		switch(thisMon) {
+			case 1 :
+			case 3 :
+			case 5 :
+			case 7 :
+			case 8 :
+			case 10 :
+			case 12:
+				System.out.println(thisMon + "월은 31일 까지입니다.");
+				break;
+			case 2 :
+				System.out.println(thisMon + "월은 29일 까지입니다.");
+				break;
+			default:
+				System.out.println(thisMon + "월은 30일 까지입니다.");		
+		}
+	
+	}
+
+}
