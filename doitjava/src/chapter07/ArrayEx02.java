a8bde7b640c9e457cd853a41f3fe8f4410062609
jiunlee <jiunlee.dev@gmail.com>
2020-09-04 15:48:31 +0900

    배열 연습

--- doitjava/src/chapter07/ArrayEx02.java
+++ doitjava/src/chapter07/ArrayEx02.java
@@ -15,10 +15,11 @@ public class ArrayEx02 {
 		for(int i = 0; i < arr2.length; i++) {
 			System.out.println("arr2["+i+"]:" + arr2[i]);
 		}
-		int[] arr3 = {1, 2, 3, 4, 5};		
+		int[] arr3 = new int[] {1, 2, 3, 4, 5};		
 		for(int i = 0; i < arr3.length; i++) {
 			System.out.println("arr3["+i+"]:" + arr3[i]);
 		}
+		arr1 = new int[]{1, 2, 3, 4, 5};
 
 	}
 
