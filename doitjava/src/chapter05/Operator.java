2d16d3ccbe1ecb2a1d62eea5a01eb6ea2d81c422
jiunlee <jiunlee.dev@gmail.com>
2020-09-09 15:36:53 +0900

    클래스 연습

---
+++ doitjava/src/chapter05/Operator.java
@@ -0,0 +1,28 @@
+package chapter05;
+
+public class Operator {
+	int add(int a, int b) {
+		return a+b;	
+	}
+	int sub(int num1, int num2) {
+		int result = num1 - num2;
+		return result;
+	}
+	int mul(int num1, int num2) {
+	return num1 + num2;
+}
+	int div(int n, int m) {
+		if(m == 0) {
+			System.out.println("0으로 나눌 수는 없습니다.");
+		}
+		return n/m;
+	}
+	void printStar() {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
