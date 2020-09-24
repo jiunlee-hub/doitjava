f39103cb43e86d39c9681fc54806d11459a430da
jiunlee <jiunlee.dev@gmail.com>
2020-09-24 15:39:36 +0900

    study

---
+++ doitjava/src/chapter13/AnonymousEx.java
@@ -0,0 +1,23 @@
+package chapter13;
+
+public class AnonymousEx {
+
+	public static void main(String[] args) {
+		Anonymous anony = new Anonymous();
+
+		anony.field.wake();
+		anony.method1();
+		anony.method2(new Person() {
+			void study() {
+				System.out.println("공부를 합니다.");
+			}
+
+			@Override
+			void wake() {
+				System.out.println("8시에 일어납니다.");
+				study();
+			}
+		});
+	}
+
+}
